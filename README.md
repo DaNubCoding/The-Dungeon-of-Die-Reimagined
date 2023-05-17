1bf279d39678a67e3a5e197c252d0e186c6e2d16
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2023-05-17 10:44:26 -0400

    initial cleanup

--- README.md
+++
@@ -1,3 +0,0 @@
-# pygame-template
-
-Just a simple template for myself to build on top of when starting a new project. 
--- src/game/main_game.py
+++ src/game/main_game.py
@@ -7,17 +7,9 @@ from src.common import *
 class MainGame(Scene):
     def setup(self) -> None:
         super().setup()
-        self.surface = pygame.Surface((100, 100), SRCALPHA)
-        self.surface.fill((255, 0, 0))
-        self.texture = Texture(self.manager.window, self.surface)
 
     def update(self) -> None:
-        # here
         super().update()
-        # or here
 
     def draw(self) -> None:
         super().draw()
-
-        self.manager.window.fill((25, 25, 25))
-        self.manager.window.render(self.texture, (100, 100))
