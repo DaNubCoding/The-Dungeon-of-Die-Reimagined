f6bf70fc1cb4e8a9ea564f59f78f4a609100f7d0
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2023-05-22 11:24:46 -0400

    separation of stacked sprite and tile

--- src/game/stacked_sprite.py
+++ src/game/stacked_sprite.py
@@ -7,29 +7,10 @@ from src.management.scene import Scene
 from src.common import *
 
 class StackedSprite(Sprite):
-    def __init__(self, scene: Scene, layer: int | Layers, faces: list[pygame.SurfaceType], pos: tuple[int, int]) -> None:
+    def __init__(self, scene: Scene, layer: int | Layers, layers: list[pygame.SurfaceType], pos: tuple[int, int]) -> None:
         super().__init__(scene, layer)
         self.pos = VEC(pos)
-        self.side_faces = faces[:-1]
-        self.top_face = faces[-1]
-        self.images = self.build_images()
         self.shader = Shader(self.manager.window, vert="res/shaders/stack.vert")
-
-    def build_images(self) -> list[pygame.SurfaceType]:
-        images = []
-
-        r = RESOLUTION
-        for i in range(15):
-            images.append(surf := pygame.Surface((64, 64)))
-            surf.blit(self.side_faces[0].subsurface((0, 64 - i * r - r, 64, r)), (0, 0))
-            surf.blit(pygame.transform.rotate(self.side_faces[1].subsurface((0, 64 - i * r - r, 64, r)), 90), (0, 0))
-            surf.blit(pygame.transform.rotate(self.side_faces[2].subsurface((0, 64 - i * r - r, 64, r)), 180), (0, 64 - r))
-            surf.blit(pygame.transform.rotate(self.side_faces[3].subsurface((0, 64 - i * r - r, 64, r)), 270), (64 - r, 0))
-
-        images.append(surf := choice(img.cobblestones).copy())
-        surf.blit(choice(img.cracks), (0, 0), special_flags=BLEND_RGB_SUB)
-
-        return images
 
     def build_layer(self, layer: int) -> None:
         StackedSpriteLayer(self.scene, self, layer)
--- src/game/tile.py
+++ src/game/tile.py
@@ -21,13 +21,35 @@ class TileManager:
             for sprite in self.stacked_sprites:
                 sprite.build_layer(i)
 
+class Tile(StackedSprite):
+    def __init__(self, scene: Scene, layer: int | Layers, faces: list[pygame.SurfaceType], pos: tuple[int, int]) -> None:
+        self.side_faces = faces[:-1]
+        self.top_face = faces[-1]
+        self.images = self.build_images()
+        super().__init__(scene, layer, self.images, pos)
+
+    def build_images(self) -> list[pygame.SurfaceType]:
+        images = []
+
+        r = RESOLUTION
+        for i in range(15):
+            images.append(surf := pygame.Surface((64, 64)))
+            surf.blit(self.side_faces[0].subsurface((0, 64 - i * r - r, 64, r)), (0, 0))
+            surf.blit(pygame.transform.rotate(self.side_faces[1].subsurface((0, 64 - i * r - r, 64, r)), 90), (0, 0))
+            surf.blit(pygame.transform.rotate(self.side_faces[2].subsurface((0, 64 - i * r - r, 64, r)), 180), (0, 64 - r))
+            surf.blit(pygame.transform.rotate(self.side_faces[3].subsurface((0, 64 - i * r - r, 64, r)), 270), (64 - r, 0))
+
+        images.append(surf := choice(img.cobblestones).copy())
+        surf.blit(choice(img.cracks), (0, 0), special_flags=BLEND_RGB_SUB)
+
+        return images
+
 class Ground(Sprite):
     def __init__(self, scene: Scene, pos: tuple[int, int]) -> None:
         super().__init__(scene, Layers.GROUND)
         image = choice(img.cobblestones).copy()
         if randint(0, 1):
             image.blit(choice(img.cracks), (0, 0))
-        image = pygame.transform.flip(image, True, True)
         (trans_surf := pygame.Surface(image.get_size())).fill((15, 15, 15))
         image.blit(trans_surf, (0, 0), special_flags=BLEND_RGB_SUB)
         self.image = Texture(self.manager.window, image, self.scene.player.camera.shader)
@@ -38,7 +60,7 @@ class Ground(Sprite):
     def draw(self) -> None:
         self.manager.window.render(self.image, self.pos)
 
-class Wall(StackedSprite):
+class Wall(Tile):
     def __init__(self, scene: Scene, pos: tuple[int, int]) -> None:
         faces: list[pygame.SurfaceType] = [choice(img.cobblestones).copy() for _ in range(5)]
         for face in faces:
