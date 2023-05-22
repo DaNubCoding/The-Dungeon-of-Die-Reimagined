22eba61fd7a945886b4f4c3699a987e03cbee693
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2023-05-22 13:21:13 -0400

    level file and OPTIMIZATION BABEEEEEEEE grouping connected walls onto the same texture/same sprite

--- src/game/tile.py
+++ src/game/tile.py
@@ -10,22 +10,41 @@ from src.common import *
 class TileManager:
     def __init__(self, scene: Scene) -> None:
         self.stacked_sprites: list[StackedSprite] = []
-        for y in range(12):
-            for x in range(16):
-                if x == 0 or x == 15 or y == 0 or y == 11:
-                    self.stacked_sprites.append(Wall(scene, (x * 64, y * 64)))
-                else:
+        self.connected: list[set] = []
+
+        _file = open("res/levels/1.txt", "r")
+        y = 0
+        while line := _file.readline():
+            for x, ch in enumerate(line):
+                if ch == ".":
                     Ground(scene, (x * 64, y * 64))
+                if ch != "#": continue
+                for chunk in self.connected:
+                    if (x - 1, y) in chunk: break
+                    if (x, y - 1) in chunk: break
+                else:
+                    self.connected.append({(x, y)})
+                    continue
+                chunk.add((x, y))
+            y += 1
+        _file.close()
+
+        for chunk in self.connected:
+            self.stacked_sprites.append(WallGroup(scene, list(chunk)))
 
         for i in range(16):
             for sprite in self.stacked_sprites:
                 sprite.build_layer(i)
 
-class Tile(StackedSprite):
-    def __init__(self, scene: Scene, layer: int | Layers, faces: list[pygame.SurfaceType], pos: tuple[int, int]) -> None:
-        self.side_faces = faces[:-1]
-        self.top_face = faces[-1]
-        super().__init__(scene, layer, self.build_images(), pos)
+class Wall:
+    def __init__(self, pos: tuple[int, int]) -> None:
+        self.pos = VEC(pos) * 64
+        self.faces: list[pygame.SurfaceType] = [choice(img.cobblestones).copy() for _ in range(5)]
+        for face in self.faces:
+            face.blit(choice(img.cracks), (0, 0), special_flags=BLEND_RGB_SUB)
+        self.side_faces = self.faces[:-1]
+        self.top_face = self.faces[-1]
+        self.images = self.build_images()
 
     def build_images(self) -> list[pygame.SurfaceType]:
         images = []
@@ -43,6 +62,17 @@ class Tile(StackedSprite):
 
         return images
 
+class WallGroup(StackedSprite):
+    def __init__(self, scene: Scene, positions: list[tuple[int, int]]) -> None:
+        corner1 = VEC(min(pos[0] for pos in positions) * 64, min(pos[1] for pos in positions) * 64)
+        corner2 = VEC(max(pos[0] for pos in positions) * 64 + 64, max(pos[1] for pos in positions) * 64 + 64)
+        images = [pygame.Surface(corner2 - corner1, SRCALPHA) for _ in range(64 // RESOLUTION)]
+        walls = [Wall(pos) for pos in positions]
+        for layer, image in enumerate(images):
+            for wall in walls:
+                image.blit(wall.images[layer], wall.pos - corner1)
+        super().__init__(scene, Layers.WALL, images, corner1)
+
 class Ground(Sprite):
     def __init__(self, scene: Scene, pos: tuple[int, int]) -> None:
         super().__init__(scene, Layers.GROUND)
@@ -58,11 +88,3 @@ class Ground(Sprite):
 
     def draw(self) -> None:
         self.manager.window.render(self.image, self.pos)
-
-class Wall(Tile):
-    def __init__(self, scene: Scene, pos: tuple[int, int]) -> None:
-        faces: list[pygame.SurfaceType] = [choice(img.cobblestones).copy() for _ in range(5)]
-        for face in faces:
-            face.blit(choice(img.cracks), (0, 0), special_flags=BLEND_RGB_SUB)
-
-        super().__init__(scene, Layers.WALL, faces, pos)
